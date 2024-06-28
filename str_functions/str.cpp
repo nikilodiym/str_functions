ddb9f5aeb036ce6ea6e178a60216cb793ad2cd83
Nikita Krenov <nikitabaram@gmail.com>
2024-06-28 12:39:28 +0300

    str_copy new function

--- str_functions/str.cpp
+++ str_functions/str.cpp
@@ -12,10 +12,16 @@ int string_length(const char* str) {
 /// </summary>
 /// <param name="str">The string</param>
 
+void string_copy(char* dest, const char* src) {
+	int i = 0;
+	while (src[i] != '\0') {
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
 
-//void toUpper(char* str);
 /// <summary>
 /// The function converts the string to lowercase
 /// </summary>
 /// <param name="str">The string</param>
-//void toLower(char* str);
