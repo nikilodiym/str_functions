362228fc8db2b65eb50422ebc9f3eb06d39b1335
Nikita Krenov <nikitabaram@gmail.com>
2024-06-28 12:49:39 +0300

    main work for this page

--- str_functions/main.cpp
+++ str_functions/main.cpp
@@ -5,6 +5,24 @@
 using namespace std;
 
 int main() {
+    int choice;
+    char str[] = "Hello, World!";
+    char buffer[50];
+
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        printf("String Length: %d\n", string_length(str));
+        break;
+    case 2:
+        printf("Copied String: %s\n", string_copy(buffer, str));
+        break;
+    default:
+        printf("Invalid choice!\n");
+        break;
+    }
 
 	return 0;
 }
